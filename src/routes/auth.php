cffe2cda68ffea624ea2d9db0dfff42dee18bdc7
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-10 14:23:36 -0600

    restablecer la contraseña, ruta bugs

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -15,7 +15,7 @@ return function (App $app) {
 
     $app->group('', function (App $app) use ($pdo) {
         // Ruta para el registro de usuarios
-        $app->post('/register', function (Request $request, Response $response) {
+        $app->post('/register', function (Request $request, Response $response )use ($pdo) {
             // Recibir los datos del usuario (como mínimo, email y contraseña)
             $data = $request->getParsedBody();
             $email = $data['email'] ?? '';
@@ -26,20 +26,67 @@ return function (App $app) {
                     'message' => 'Datos inválidos'
                 ]);
             }
+
+            // Hasheamos la contraseña
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            $stmt = $pdo->prepare("INSERT INTO users (email, password) VALUES (:email, :password)");
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password', $hashedPassword);
+            $stmt->execute();
+
+            return $response->withJson([
+                'message' => 'Usuario registrado exitosamente'
+            ]);
         });
     
         // Ruta para iniciar sesión
-        $app->post('/login', function (Request $request, Response $response) {
-            // Implementar la lógica de inicio de sesión aquí
-            // Deberás recibir los datos de inicio de sesión, validarlos contra tu sistema,
-            // crear un token o una sesión si las credenciales son válidas y retornar una respuesta
-            return $response;
+        $app->post('/login', function (Request $request, Response $response) use ($pdo){
+            // Datos de inicio de sesion
+            $data = $request->getParsedBody();
+            $email = $data['email'] ?? '';
+            $password = $data['password'] ?? '';
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL) || empty($password)) {
+                return $response->withStatus(400)->withJson([
+                    'message' => 'Datos inválidos'
+                ]);
+            }
+
+            // Revisar las credenciales en la base de datos
+            $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (!$user || !password_verify($password, $user['password'])) {
+                return $response->withStatus(401)->withJson([
+                    'message' => 'Credenciales inválidas'
+                ]);
+            }
+            // Aquí se debe crear un token o una sesión si las credenciales son válidas y retornar una respuesta
+            return $response->withJson([
+                'message' => 'Inicio de sesión exitoso'
+            ]);
         });
     })->add(new AuthMiddleware()); // Aquí se aplica el middleware de autenticación a las rutas del grupo
     
     // Ruta para restablecer la contraseña
-    $app->post('/password-reset', function (Request $request, Response $response) {
-        // Implementar la lógica de restablecimiento de contraseña aquí
+    $app->post('/password-reset', function (Request $request, Response $response) use ($pdo) {
+        // Recibir el correo electrónico del usuario
+        $data = $request->getParsedBody();
+        $email = $data['email'] ?? '';
+
+        // Validar el correo electrónico
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return $response->withStatus(400)->withJson([
+                'message' => 'Correo inválido'
+            ]);
+        }
+
+        // Generar un token para el restablecimiento de la contraseña
+        $token = bin2hex(random_bytes(32));
+        
         // Normalmente, esto implicará enviar un correo electrónico al usuario con un token
         // que luego pueda usar para cambiar su contraseña
         return $response;
